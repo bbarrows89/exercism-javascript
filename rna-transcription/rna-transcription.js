d76f0c2cc72f63e71059fa0220f690a9587ae013
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-02-25 18:54:53 -0800

    Now using conditionals, made it easier to throw errors for invalid input

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -2,20 +2,19 @@ var DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(dna) {
   var rnaString = [];
+  if (dna.length === 0) {
+    throw Error('Invalid input');
+  }
   for (var i = 0; i < dna.length; i++) {
-    switch(dna[i]) { //check each character in DNA string)
-      case 'G':
-        rnaString.push('C');
-        continue;
-      case 'C':
+    if (dna[i] == 'G') { //check each character in DNA string)
+      rnaString.push('C');
+    } else if (dna[i] == 'C'){
         rnaString.push('G');
-        continue;
-      case 'T':
-        rnaString.push('A');
-        continue;
-      case 'A':
-        rnaString.push('U');
-    } // end switch
+    } else if (dna[i] == 'T'){
+      rnaString.push('A');
+    } else if (dna[i] == 'A'){
+      rnaString.push('U');
+    } else throw Error('Invalid input')
   } // end for loop
   return rnaString.join(''); 
 }
